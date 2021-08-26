337304a48d115d4fdca6274f9949b4970f4de5d4
AlexRunin <shurupamg@gmail.com>
2021-08-26 17:58:07 +0300

    Update_26_08_21
    Update_26_08_21

--- Stone Age Group1/Assets/Scripts/MyPlayerMovement.cs
+++ Stone Age Group1/Assets/Scripts/MyPlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D))] // запрещяет удалять определенного компонента
 public class MyPlayerMovement : MonoBehaviour
@@ -17,7 +18,9 @@ public class MyPlayerMovement : MonoBehaviour
     private float currentPlayerSpeed; // она по дефолту = 0, в нее сохраняем 
     private Rigidbody2D rb;
     private bool groundCheck; // проверка земли
-    
+
+    private string lastObjectEated;
+    Vector3 defaultPosition;
 
 
     private void Awake()
@@ -25,11 +28,42 @@ public class MyPlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = animObject.GetComponent<SpriteRenderer>(); // плучаем ссылку
         animator = animObject.GetComponent<Animator>(); // получаем компонент
+        defaultPosition = this.transform.position;
     }
 
     // Движение
     private void FixedUpdate()
     {
+        /*
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * playerSpeed * Time.deltaTime;
+
+        currentPlayerSpeed = move.x * playerSpeed;
+
+        if (currentPlayerSpeed > 0)
+        {
+            RightMove();
+        }
+        else if (currentPlayerSpeed < 0)
+        {
+            LeftMove();
+        }
+
+        if (move.y > 0)
+        {
+            Jump();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            //Throw();
+        }
+        */
+       
         // ДЗ добавить enum
         // Сделать флип через Флип
         // 4 способа UIExample
@@ -78,9 +112,77 @@ public class MyPlayerMovement : MonoBehaviour
         
     }
 
+    public void EateFruit(GameObject banana)
+    {
+        Debug.Log("Eat banana");
+
+        GameObject.Destroy(banana);
+
+        //ChangeHealth(-0.15f);
+    }
+
+    public void EateMushroom(GameObject mushroom)
+    {
+        Debug.Log("Eating mushrooms");
+
+        GameObject.Destroy(mushroom);
+
+        //ChangeHealth(0.15f);
+    }
+
+    public void EateOrange(GameObject orange)
+    {
+        Debug.Log("Eating orange");
+
+        GameObject.Destroy(orange);
+
+        //ChangeHealth(0.15f);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         groundCheck = true;
+
+        switch (collision.tag)
+        {
+            case "Banana":
+                {
+                    EateFruit(collision.gameObject);
+                    lastObjectEated = collision.tag;
+                }
+                break;
+            case "Mushroom":
+                {
+                    EateMushroom(collision.gameObject);
+                    lastObjectEated = collision.tag;
+                }
+                break;
+            case "Orange":
+                {
+                    EateOrange(collision.gameObject);
+                    lastObjectEated = collision.tag;
+                }
+                break;
+            case "SpecialMushroom":
+                {
+                    EateMushroom(collision.gameObject);
+                    lastObjectEated = collision.tag;
+                }
+                break;
+            case "Water":
+                {
+                    if (lastObjectEated != "SpecialMushroom")
+                    {
+                        this.transform.position = defaultPosition;
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene(0);
+                    }
+                    lastObjectEated = collision.tag;
+                }
+                break;
+            
+        }
     }
 
 
